c3ca4fce206494e3f24038c585fd6092e5ed6b1d
spacemen0 <frakctures@outlook.com>
2024-02-11 20:51:26 +0100

    add code files

---
+++ java-module/src/main/java/se/liu/ida/hello/TimeRetriever.java
@@ -0,0 +1,16 @@
+package se.liu.ida.hello;
+
+import org.joda.time.LocalTime;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class TimeRetriever {
+    public static String getTimeMessage() {
+        Logger logger = LogManager.getLogger(HelloMaven.class);
+        LocalTime localTime = new LocalTime();
+
+        logger.info("Requested time string: " + localTime.toString());
+        return "TODO list: " + localTime.toString();
+    }
+}
