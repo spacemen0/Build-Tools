463edaa3cc3f1298cdf2b2b049b3f0cc295dbac7
spacemen0 <frakctures@outlook.com>
2024-02-15 15:45:54 +0100

    commit

--- java-module/src/main/java/se/liu/ida/hello/TimeRetriever.java
+++ java-module/src/main/java/se/liu/ida/hello/TimeRetriever.java
@@ -3,12 +3,16 @@ package se.liu.ida.hello;
 import org.joda.time.LocalTime;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
 
 public class TimeRetriever {
     public static String getTimeMessage() {
-        Logger logger = LogManager.getLogger(HelloMaven.class);
+        Logger logger = LogManager.getLogger(TimeRetriever.class);
         LocalTime localTime = new LocalTime();
-
+        LoggerContext context = (LoggerContext) LogManager.getContext(false);
+        Configuration config = context.getConfiguration();
+        logger.error("Configuration Source: " + config.getConfigurationSource().toString());
         logger.info("Requested time string: " + localTime.toString());
         return "TODO list: " + localTime.toString();
     }
